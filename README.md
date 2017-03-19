0aafddf09d0fcb9ed0b40fe9a560fd9fd3ad5d54
Jason Fuller <JasonSFuller@gmail.com>
2017-03-18 23:53:23 -0400

    added umask

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # dotfiles
 
-My personal dotfiles
+My personal dot files
+
+## Installation
+
+```bash
+mkdir ~/src
+cd    ~/src
+git clone https://github.com/JasonSFuller/dotfiles
+cd dotfiles
+./install.sh
+```
--- bashrc.d/disable-auto-logout.sh
+++ bashrc.d/disable-auto-logout.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# IMPORTANT: This is not intended to be executed directly, even 
+# though I've added a she-bang at the top.  $TMOUT is usually 
+# set as readonly, which means you'll have to use gdb, and gdb 
+# must attach to your login shell.  If run as a script, it will 
+# be a child process and the TMOUT variable won't be unset 
+# properly.  I could probably be more clever and find it, but 
+# this "works for me" so... meh.
+
 function killwithfire {
   if ! which gdb >&/dev/null; then
     echo "ERROR: gdb could not be found." >&2
