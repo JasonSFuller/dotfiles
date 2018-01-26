a1b3c225a8ddf3695191eb3bad0b8024f6c764b4
Jason Fuller <JasonSFuller@gmail.com>
2018-01-26 13:27:18 -0500

    bugfix: bash history across machines with shared home

--- bashrc.d/history.sh
+++ bashrc.d/history.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+if [[ ! -d "$HOME/.bash_history.d" ]]; then
+  mkdir -p "$HOME/.bash_history.d"
+fi
+
+if [[ -z "$HOSTNAME" ]]; then
+  HOSTNAME=$(hostname -f)
+fi
+
+export HISTFILE="$HOME/.bash_history.d/$HOSTNAME"
 export HISTTIMEFORMAT='%Y-%m-%d+%H:%M:%S  '
