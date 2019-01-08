e39e36ae3e737c4259f4ae5178c465fb45c3b9aa
Jason Fuller <JasonSFuller@gmail.com>
2019-01-08 13:14:45 -0500

    more history and terminal tweaks

--- bashrc.d/history.sh
+++ bashrc.d/history.sh
@@ -6,5 +6,7 @@ fi
 
 HISTHOSTNAME=$(hostname -f)
 
-export HISTFILE="$HOME/.bash_history.d/$HISTHOSTNAME"
-export HISTTIMEFORMAT='%Y-%m-%d+%H:%M:%S  '
+export HISTFILE="$HOME/.bash_history.d/$HISTHOSTNAME" # for my roaming home dir (multiple systems)
+export HISTTIMEFORMAT='%Y-%m-%d+%H:%M:%S  '           # timestamp my commands
+export HISTSIZE=100000                                # keep an unreasonably large history
+export HISTFILESIZE=100000
---
+++ bashrc.d/terminal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+stty -ixon        # disable ctrl-s and ctrl-q
+shopt -s autocd   # cd into directories merely by typing the name
