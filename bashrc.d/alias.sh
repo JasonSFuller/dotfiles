32eed202beb1dd05af37ba758c15c2dd11f79a37
Jason Fuller <JasonSFuller@gmail.com>
2019-04-24 15:21:03 -0400

    macos specific tweaks

--- bashrc.d/alias.sh
+++ bashrc.d/alias.sh
@@ -3,7 +3,12 @@
 #alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
-alias l='ls -l --color=auto'
-#alias ll='ls -lA --color=auto'
-alias ll='if [[ $PWD == $HOME ]]; then ls -l --color=auto; else ls -la --color=auto; fi'
-alias ls='ls --color=auto'
+if uname -s | grep -q Darwin; then # MacOS specific
+  alias l='ls -lG'
+  alias ll='if [[ $PWD == $HOME ]]; then ls -lG; else ls -laG; fi'
+  alias ls='ls -G'
+else
+  alias l='ls -l --color=auto'
+  alias ll='if [[ $PWD == $HOME ]]; then ls -l --color=auto; else ls -la --color=auto; fi'
+  alias ls='ls --color=auto'
+fi
--- bashrc.d/terminal.sh
+++ bashrc.d/terminal.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 if [[ $- =~ i ]]; then stty -ixon; fi # disable ctrl-s and ctrl-q for interactive shells
-shopt -s autocd   # cd into directories merely by typing the name
+
+if ! uname -s | grep -q Darwin; then # MacOS bash doesn't support these
+  shopt -s autocd # cd into directories merely by typing the name
+fi
+
