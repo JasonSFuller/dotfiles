859a9526d6294313a0693187cb23fd5cc2cb4fc5
Jason Fuller <JasonSFuller@gmail.com>
2019-04-29 14:53:08 -0400

    add function to scope vars, plus treat/color host and user separately

--- bashrc.d/prompt.sh
+++ bashrc.d/prompt.sh
@@ -1,27 +1,35 @@
-# NOTE: many of the checks here are borrowed directly from RHEL 7 /etc/bashrc
+function colorize_prompt {
+  local red=$(tput setaf 1)
+  local grn=$(tput setaf 2)
+  local yel=$(tput setaf 3)
+  local blu=$(tput setaf 4)
+  local rst=$(tput sgr0)
+  local host='\h'
+  local user='\u'
+  local wdir='\W'
+  case "${HOSTNAME:3:1}" in # get 4th char
+    [sStTdD]) host="${grn}${host}${rst}" ;; # sandbox, test, and dev
+    [qQuU])   host="${yel}${host}${rst}" ;; # qa and uat
+    [pP])     host="${red}${host}${rst}" ;; # prod
+    *)
+      if grep "^${HOSTNAME}\$" "$HOME/.ssh/ssh-agent-hosts" &>/dev/null; then 
+        host="${blu}${host}${rst}" # my local pc
+      fi
+      ;;
+  esac
+  if [[ "${USER}" == 'root' ]]; then user="${red}${user}${rst}"; fi
+  PS1="[${user}@${host} ${wdir}]\\$ "
+}
 
-# Is this an interactive shell (with color)?
+# Is this an interactive shell?
 if shopt -q login_shell && tty -s; then
-  # Is PS1 empty or set to a default?  If so, make it a little nicer.
+  # Is PS1 empty or set to a default?
   #   '\s-\v\$ '    = bash default on RHEL systems
   #   '\h:\W \u\$ ' = MacOS default
   if [[ -z "$PS1" || "$PS1" == '\s-\v\$ ' || "$PS1" == '\h:\W \u\$ ' ]]; then
     PS1='[\u@\h \W]\$ '
   fi
-  # Find the 4th character in the hostname:
-  #   ---green---  --yellow--   --red--
-  #   s = sandbox  q = qa       p = prod
-  #   t = test     u = uat
-  #   d = dev
-  # Or if you're my local PC (with my ssh-agent) = blue
-  case "${HOSTNAME:3:1}" in
-    [sStTdD]) PS1="$(tput setaf 2)$PS1$(tput sgr0)" ;; # green
-    [qQuU])   PS1="$(tput setaf 3)$PS1$(tput sgr0)" ;; # yellow
-    [pP])     PS1="$(tput setaf 1)$PS1$(tput sgr0)" ;; # red
-    *)
-      if grep "^${HOSTNAME}\$" "$HOME/.ssh/ssh-agent-hosts" &>/dev/null; then 
-        PS1="$(tput setaf 6)$PS1$(tput sgr0)" # cyan
-      fi
-      ;;
-  esac
+  colorize_prompt
 fi
+
+unset -f colorize_prompt
