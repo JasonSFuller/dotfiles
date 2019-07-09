c71c6862c0f65d568f84e450a9c5ae95d118ae96
Jason Fuller <JasonSFuller@gmail.com>
2019-07-09 11:51:19 -0400

    cleaner

--- bashrc.d/terraform.sh
+++ bashrc.d/terraform.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [[ -d "$HOME/terraform" && ! "$PATH" =~ terraform ]]; then
-  export PATH=$PATH:$HOME/terraform
-fi
-
-if which terraform &>/dev/null; then
-  complete -C /home/jfuller/terraform/terraform terraform
+if [[ -d "$HOME/terraform" ]]; then
+  if [[ ! "$PATH" =~ terraform ]]; then
+    export PATH=$PATH:$HOME/terraform
+  fi
+  if which terraform &>/dev/null; then
+    complete -C $(which terraform) terraform
+  fi
 fi
