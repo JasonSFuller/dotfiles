114de8aabfa54dd5f74ca159ba426773bdecebf4
Jason Fuller <JasonSFuller@gmail.com>
2019-07-09 15:45:19 -0400

    removed TF path stuff

--- bashrc.d/terraform.sh
+++ bashrc.d/terraform.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-if [[ -d "$HOME/terraform" ]]; then
-  if [[ ! "$PATH" =~ terraform ]]; then
-    export PATH=$PATH:$HOME/terraform
-  fi
-  if which terraform &>/dev/null; then
-    complete -C $(which terraform) terraform
-  fi
+if which terraform &>/dev/null; then
+  complete -C $(which terraform) terraform
 fi
+
