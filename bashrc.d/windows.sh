4776e7f6253f5d00f5407409db8426ee14f90d26
Jason Fuller <JasonSFuller@gmail.com>
2020-02-18 18:04:08 -0500

    detect wsl1 and start ssh

--- bashrc.d/windows.sh
+++ bashrc.d/windows.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# detect cygwin via the mingw terminal emulation (not ideal)
 if [[ "$(uname -s)" =~ ^MINGW ]]; then
   echo "Ugh. I'm in Windows. Do 'special' things."
+fi
+
+# detect WSL1
+# TODO differentiate between 1 and 2 somehow
+if grep -qiE '^linux\b.*\b(microsoft|wsl)\b' /proc/version; then
+  echo "Less painful, but still Windows."
+  sudo service ssh status &> /dev/null
+  if [[ $? -eq 3 ]]; then
+    sudo service ssh start
+  fi
 fi
