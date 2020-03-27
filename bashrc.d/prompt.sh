dc59c2e9113d57dcd3361fbd35533f6f7a5577fc
Jason Fuller <JasonSFuller@gmail.com>
2020-03-27 15:33:26 -0400

    ubuntu 18.04 doesnt set shopt login_shell

--- bashrc.d/prompt.sh
+++ bashrc.d/prompt.sh
@@ -1,10 +1,4 @@
 # Is this an interactive shell?
-if shopt -q login_shell && tty -s; then
-  # Is PS1 empty or set to a default?
-  #   '\s-\v\$ '    = bash default on RHEL systems
-  #   '\h:\W \u\$ ' = MacOS default
-  if [[ -z "$PS1" || "$PS1" == '\s-\v\$ ' || "$PS1" == '\h:\W \u\$ ' ]]; then
-    PS1='[\u@\h \W]\$ '
-  fi
+if tty -s; then
+  PS1='[\u@\h \W]\$ '
 fi
-
