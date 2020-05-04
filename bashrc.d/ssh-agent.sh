7a86a17e80af04e6832b52b68365fb7a8263913f
Jason Fuller <JasonSFuller@gmail.com>
2020-05-04 10:51:10 -0400

    add support for gnome-keyring-daemon to act as ssh-agent

--- bashrc.d/ssh-agent.sh
+++ bashrc.d/ssh-agent.sh
@@ -1,10 +1,22 @@
 function load_ssh_agent {
 
-  echo "loading agent..."
-  ssh-agent > "$HOME/.ssh/ssh-agent"
-  chmod 0600  "$HOME/.ssh/ssh-agent"
-  cat "$HOME/.ssh/ssh-agent"
-  source "$HOME/.ssh/ssh-agent"
+  # if you're on a distro running a desktop with gnome keyring, check for the
+  # daemon.  it should be started (by pam) when you login and $SSH_AUTH_SOCK
+  # _should_ already be defined... BUT if you've made it here, the socket is 
+  # invalid, so...
+  if builtin command -v gnome-keyring-daemon &>/dev/null &&
+     pgrep -f 'gnome-keyring-daemon\b.*\s(--login|-l)\b' &>/dev/null
+  then
+    echo "starting gnome-keyring-daemon..."
+    gnome-keyring-daemon -s   > "$HOME/.ssh/ssh-agent"
+  else
+    echo "starting ssh-agent..."
+    ssh-agent | grep -v ^echo > "$HOME/.ssh/ssh-agent"
+  fi
+
+  chmod 0600 "$HOME/.ssh/ssh-agent"
+  cat        "$HOME/.ssh/ssh-agent"
+  source     "$HOME/.ssh/ssh-agent"
 
 }
 
@@ -17,48 +29,20 @@ function check_ssh_agent {
     fi
   fi
 
-  if [[ ! -f "$HOME/.ssh/ssh-agent" ]]; then
+  if [[ ! -r "$HOME/.ssh/ssh-agent" ]]; then
     if ! touch "$HOME/.ssh/ssh-agent"; then
       echo "ERROR: cannot create ssh-agent env file" >&2
       return 1
     fi
   fi
 
-  if [[ ! -r "$HOME/.ssh/ssh-agent" ]]; then
-     echo "ERROR: cannot read ssh-agent env file" >&2
-     return 1
-  fi
-    
   source "$HOME/.ssh/ssh-agent"
 
-  if [[ -z "$SSH_AUTH_SOCK" ]]; then
-    echo "INFO: \$SSH_AUTH_SOCK is empty"
-    load_ssh_agent
-    return
-  fi 
-
   if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
-    echo "INFO: \$SSH_AUTH_SOCK is not a socket"
+    echo "INFO: no socket found (SSH_AUTH_SOCK=$SSH_AUTH_SOCK)"
     load_ssh_agent
-    return 
   fi
 
-  if [[ -z "$SSH_AGENT_PID" ]]; then
-    echo "INFO: \$SSH_AGENT_PID is empty"
-    load_ssh_agent
-    return
-  fi
-
-  if [[ ! "$SSH_AGENT_PID" =~ ^[0-9]+$ ]]; then
-    echo "ERROR: \$SSH_AGENT_PID is non-numeric ($SSH_AGENT_PID)"
-    return 1
-  fi
-    
-  if ! pgrep ssh-agent | grep -q "^${SSH_AGENT_PID}\$"; then
-    echo "INFO: PID ($SSH_AGENT_PID) not ssh-agent or not found"
-    load_ssh_agent
-    return
-  fi
 }
 
 # On hosts where you want the agent to start:
