bfbdfccc8596898ebf7bb0d044901b314b5adf60
Jason Fuller <JasonSFuller@gmail.com>
2020-05-04 07:41:21 -0400

    slash cleanup can be done with single sed; whitespace+comment clean up

--- bashrc.d/path.sh
+++ bashrc.d/path.sh
@@ -3,22 +3,30 @@
 # accepts an array of directories to be added to $PATH
 
 function add_paths {
+
   #local -n dirs="$1" # "-n" only in bash 4.3+
   local dirs=("$@")
   local paths
 
   for dir in "${dirs[@]}"
   do
-    dir=$(echo "$dir" | tr -s / | sed 's:/$::') # remove extra+trailing slashes
-    if [[ ! -d "$dir" ]]; then continue; fi
+
+    dir=$(echo "$dir" | sed 's://*:/:g; s:/$::') # remove extra+trailing slashes
+    if [[ ! -d "$dir" ]]; then continue; fi      # skip nonexistent dir
+
     # $PATH is a string and not an array, so convert it for precision and safety
     IFS=$':' read -r -d '' -a paths <<< "$PATH"
-    for path in "${paths[@]}" # loop through $PATH
-    do # and if the dir is already in $PATH, skip to the next $dir
-      path="${path%%[[:space:]]*}" # trim trailing white space
-      if [[ "$dir" == "$path" ]]; then continue 2; fi
+
+    for path in "${paths[@]}"
+    do
+      path="${path%%[[:space:]]*}"     # trim trailing white space
+      if [[ "$dir" == "$path" ]]; then # if already in $PATH, skip
+        continue 2
+      fi
     done
+
     export PATH="$PATH:$dir"
+
   done
 }
 
