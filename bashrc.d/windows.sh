aed49df11241dbd1b385fa4854639938e6482250
Jason Fuller <JasonSFuller@gmail.com>
2021-12-14 17:08:07 -0500

    detect wsl2

--- bashrc.d/windows.sh
+++ bashrc.d/windows.sh
@@ -5,12 +5,15 @@ if [[ "$(uname -s)" =~ ^MINGW ]]; then
   echo "Ugh. I'm in Windows. Do 'special' things."
 fi
 
-# detect WSL1
-# TODO differentiate between 1 and 2 somehow
+# detect wsl
 if grep -qiE '^linux\b.*\b(microsoft|wsl)\b' /proc/version; then
-  echo "Less painful, but still Windows."
-  sudo service ssh status &> /dev/null
-  if [[ $? -eq 3 ]]; then
-    sudo service ssh start
+  # wsl2 (and windows terminal) is considerably less stupid, 
+  # so if you find v2, do nothing.  otherwise, startup sshd.
+  if ! grep -qiE '\bwsl2\b' /proc/version; then
+    echo "Detected WSL; starting sshd..."
+    sudo service ssh status &> /dev/null
+    if [[ $? -eq 3 ]]; then
+      sudo service ssh start
+    fi
   fi
 fi
