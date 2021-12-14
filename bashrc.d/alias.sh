9a0b2f9610d95c1e3f139b1f1de19cff6c99450c
Jason Fuller <JasonSFuller@gmail.com>
2021-12-14 17:26:12 -0500

    add alias for bat

--- bashrc.d/alias.sh
+++ bashrc.d/alias.sh
@@ -14,3 +14,10 @@ alias l.='ls -lad .*'
 alias ll='ls -la'
 alias now='date +%Y%m%d%H%M%S'
 
+if builtin command -v batcat &>/dev/null; then
+  alias cat='batcat --decorations never --paging never'
+elif builtin command -v bat &>/dev/null; then
+  alias cat='bat --decorations never --paging never'
+fi
+
+
