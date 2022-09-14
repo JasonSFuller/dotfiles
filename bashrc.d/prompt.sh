5ab341637620db09e33ec51ed938b9e00754fb67
Jason Fuller <JasonSFuller@gmail.com>
2022-09-13 23:58:57 -0400

    moar shellcheck

--- bashrc.d/prompt.sh
+++ bashrc.d/prompt.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # TODO look at /usr/lib/git-core/git-sh-prompt for inspiration on git prompt
 
 # Is this an interactive shell?
@@ -21,12 +23,12 @@ function generate_prompt {
     if grep -qi ubuntu /etc/*release &>/dev/null; then
       local title="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h:\w\a\]"
     fi
-
-    local red="\[$(tput bold; tput setaf 1)\]"
-    local grn="\[$(tput bold; tput setaf 2)\]"
-    local yel="\[$(tput bold; tput setaf 3)\]"
-    local blu="\[$(tput bold; tput setaf 4)\]"
-    local rst="\[$(tput sgr0)\]"
+    local grn blu rst
+    #red="\[$(tput bold; tput setaf 1)\]"
+    grn="\[$(tput bold; tput setaf 2)\]"
+    #yel="\[$(tput bold; tput setaf 3)\]"
+    blu="\[$(tput bold; tput setaf 4)\]"
+    rst="\[$(tput sgr0)\]"
 
     PS1="$title[$grn\u@\h$rst $blu\W$rst]\$ " # add title and color
 
