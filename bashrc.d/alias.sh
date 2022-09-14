f019e297067ddaf81dcc1fc7674b0c2b02833894
Jason Fuller <JasonSFuller@gmail.com>
2022-09-13 23:48:36 -0400

    mac sucks, and i'm never working on it again

--- bashrc.d/alias.sh
+++ bashrc.d/alias.sh
@@ -3,12 +3,8 @@
 #alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
-if uname -s | grep -q Darwin; then # MacOS (BSD-based OS tools vs GNU coreutils)
-  alias ls='ls -G'
-else
-  alias ls='ls --color=auto'
-  alias grep='grep --color=auto'
-fi
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
 alias l='ls -l'
 alias l.='ls -lad .*'
 alias ll='ls -la'
